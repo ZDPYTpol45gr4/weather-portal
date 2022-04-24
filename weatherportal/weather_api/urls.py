553ee670c095e975f15e132f033a8885aa3a0564
Arkadiusz Cieply <152081@stud.prz.edu.pl>
2022-04-24 15:58:34 +0200

    Add simple  weather view, showing temperature for 7 days

---
+++ weatherportal/weather_api/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('test1/', views.get_current_weather_by_location, name='weather_api_current_weather_view'),
+    path('test2/', views.weather_7_days_view, name='weather_api_weather_6_days_view'),
+]
--- weatherportal/weather_api/views.py
+++ weatherportal/weather_api/views.py
@@ -1,3 +1,73 @@
-from django.shortcuts import render
+import os
 
-# Create your views here.
+from django.shortcuts import render
+import requests
+
+API_KEY = os.getenv('API_KEY')
+
+
+def get_location(location):
+    response_location_coords = requests.get(
+        f'http://api.openweathermap.org/geo/1.0/direct?q={location}&limit=5&appid={API_KEY}'
+        )
+
+    get_data_coords = response_location_coords.json()
+    return get_data_coords[0]
+
+
+def get_all_data_forecast_weather_by_location(request):
+    """
+    Get weather data from api:
+        Current weather
+        Minute forecast for 1 hour
+        Hourly forecast for 48 hours
+        Daily forecast for 7 days
+        National weather alerts
+        Historical weather data for the previous 5 days
+    """
+
+    data_coords = get_location("London")
+
+    lat = data_coords['lat']
+    lon = data_coords['lon']
+
+    response_weather = requests.get(
+        f'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&units=metric&appid={API_KEY}'
+        )
+    get_data_weather = response_weather.json()
+
+    return get_data_weather
+
+
+def get_current_weather_by_location(location):
+    """
+    Get current weather from specific location
+    """
+    data_coords = get_location(location)
+
+    lat = data_coords['lat']
+    lon = data_coords['lon']
+
+    response_weather = requests.get(
+        f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_KEY}'
+        )
+    get_data_weather = response_weather.json()
+
+    return get_data_weather
+
+
+def weather_7_days_view(request):
+    data = get_all_data_forecast_weather_by_location('London')
+    temperature = [val['temp'] for val in data['daily']]
+    day1 = temperature[1]
+    day2 = temperature[2]
+    day3 = temperature[3]
+    day4 = temperature[4]
+    day5 = temperature[5]
+    day6 = temperature[6]
+    day7 = temperature[7]
+    ctx = {'temperature': temperature, 'day1': day1, 'day2': day2,
+           'day3': day3, 'day4': day4, 'day5': day5, 'day6': day6,
+           'day7': day7
+           }
+    return render(request, 'weather_api/current_weather.html', ctx)
