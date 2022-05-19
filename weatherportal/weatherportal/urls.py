f23b104aa7c27e3e31b7fddd19cbf52b66ed3e41
ZDPYTpol45gr4 <104261337+ZDPYTpol45gr4@users.noreply.github.com>
2022-05-19 20:58:45 +0200

    Update urls.py
    Changed due to the problems with marging

--- weatherportal/weatherportal/urls.py
+++ weatherportal/weatherportal/urls.py
@@ -1,20 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 
-<<<<<<< HEAD
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('register.urls')),
-=======
-from register import views as v
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('weather/', include('weather_api.urls')),
-    path("register/", v.register, name="register"),
-    path('', include("main.urls")),
-    path('', include("django.contrib.auth.urls")),
->>>>>>> 027a610dbcb7aca3c3ade524bf164188cba3e864
+    path('', include('register.urls')),
 ]
-
